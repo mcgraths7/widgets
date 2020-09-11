57481339f85ca6c35764a5e447c244f90bf3895a
mcgraths7 <mcgrath.steven7@gmail.com>
2020-09-11 11:17:43 -0400

    added changing document title when links clicked or page is navigated to manually

--- src/components/nav/Link.jsx
+++ src/components/nav/Link.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const Link = ({ href, idx, setActive, isActive, children }) => {
+const Link = ({ href, label, idx, setActive, isActive, children }) => {
   const onLinkClicked = (e) => {
+    if (e.metaKey || e.ctrlKey) {
+      return;
+    }
     e.preventDefault();
     window.history.pushState({}, '', href)
     setActive(idx);
+    document.title = `Widgets | ${label}`;
     const navEvent = new PopStateEvent('popstate');
     window.dispatchEvent(navEvent);
   }
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -6,21 +6,27 @@ const Navbar = ({ navItems }) => {
 
   useEffect(() => {
     const path = window.location.pathname;
+    const label = path.split('/')[1];
     switch(path) {
       case '/':
         setActiveItem(0);
+        document.title = 'Widgets | Accordion';
         break
       case '/list':
         setActiveItem(1);
+        document.title = `Widgets | ${label}`;
         break;
       case '/dropdown':
         setActiveItem(2);
+        document.title = `Widgets | ${label}`;
         break;
       case '/translate':
         setActiveItem(3);
+        document.title = `Widgets | ${label}`;
         break;
       default: 
         setActiveItem(0);
+        document.title = 'Widgets';
     }
   }, [])
 
@@ -30,6 +36,7 @@ const Navbar = ({ navItems }) => {
       isActive={activeItem === idx}
       href={navItem.href}
       idx={idx}
+      label={navItem.label}
       setActive={setActiveItem}
     >
       {navItem.label}
